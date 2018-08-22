81988ec5cb30ad7dc1ed6eddb79cf8ff12671fe4
Alicia Collins <alicia.collins60@me.com>
2018-08-21 22:18:08 -0700

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var bodyParser = require("body-parser")
 var logger = require("morgan");
 var mongoose = require("mongoose");
@@ -12,7 +12,7 @@ var cheerio = require('cheerio');
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
- //mongoose.Promise = Promise;
+//mongoose.Promise = Promise;
 // mongoose.connect(MONGODB_URI);
 mongoose.connect("mongodb://localhost/Scrapping");
 
@@ -30,103 +30,108 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout:"main"}));
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: "main" }));
 app.set('view engine', '.hbs');
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/scrapping");
 
 
-app.get("/scrape", function(req,res){
-    
-   axios.get("https://www.azcardinals.com/news/").then(function(response){
-       
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://www.azcardinals.com/news/").then(function (response) {
+
         var $ = cheerio.load(response.data);
 
-    $(".d3-l-col__col-3").each(function(i,element){
-        
-        var result = {};
-///use this or element
-        result.title =$(element).find('a').attr("title");
-        result.link = "https://www.azcardinals.com/" + $(element).find('a').attr("href");
-        result.summary =$(element).find(".d3-o-media-object__summary").text().trim();
-        result.img =$(element).find("img").attr('src');
+        $(".d3-l-col__col-3").each(function (i, element) {
 
-        Article.create(result)
-        .then(function(dbArticle){
-            console.log("dbArticle2222",dbArticle)
-        })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-            });
-    });
-   // console.log("result",result);
-    res.send("Scrape Complete");
+            var result = {};
+            ///use this or element
+            result.title = $(element).find('a').attr("title");
+            result.link = "https://www.azcardinals.com/" + $(element).find('a').attr("href");
+            result.summary = $(element).find(".d3-o-media-object__summary").text().trim();
+            result.img = $(element).find("img").attr('src');
+
+            Article.create(result)
+                .then(function (dbArticle) {
+                    console.log("dbArticle2222", dbArticle)
+                })
+                .catch(function (err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+        });
+        
+        res.send("Scrape Complete");
     });
 });
 
-app.get("/", function(req,res){
-    Article.find({}).then(function(dbArticles){
-        console.log("dbArticles",dbArticles);
-        res.render("index", {articles: dbArticles})
-    })
-})
+// app.get("/", function (req, res) {
+//     Article.find({})
+//     .then(function (dbArticles) {
+//         console.log("dbArticles", dbArticles);
+//         res.render("index", { articles: dbArticles });
+//     })
+//         .catch(function (err) {
+//             // If an error occurred, send it to the client
+//             res.json(err);
+//         });
+// });
 
 
-// Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//     // Grab every document in the Articles collection
-//     db.Article.find({})
-//       .then(function(dbArticle) {
-//         // If we were able to successfully find Articles, send them back to the client
-//         res.json(dbArticle);
-//       })
-//       .catch(function(err) {
-//         // If an error occurred, send it to the client
-//         res.json(err);
-//       });
-//   });
-  
-//   // Route for grabbing a specific Article by id, populate it with it's note
-//   app.get("/articles/:id", function(req, res) {
+//why am I only getting the title and link summary and image undefined when sent to html
+app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+// app.get("/articles/:id", function (req, res) {
 //     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
 //     db.Article.findOne({ _id: req.params.id })
-//       // ..and populate all of the notes associated with it
-//       .populate("note")
-//       .then(function(dbArticle) {
-//         // If we were able to successfully find an Article with the given id, send it back to the client
-//         res.json(dbArticle);
-//       })
-//       .catch(function(err) {
-//         // If an error occurred, send it to the client
-//         res.json(err);
-//       });
-//   });
-  
-//   // Route for saving/updating an Article's associated Note
-//   app.post("/articles/:id", function(req, res) {
+//         // ..and populate all of the notes associated with it
+//         .populate("note")
+//         .then(function (dbArticle) {
+//             // If we were able to successfully find an Article with the given id, send it back to the client
+//             res.json(dbArticle);
+//         })
+//         .catch(function (err) {
+//             // If an error occurred, send it to the client
+//             res.json(err);
+//         });
+// });
+
+// // Route for saving/updating an Article's associated Note
+// app.post("/articles/:id", function (req, res) {
 //     // Create a new note and pass the req.body to the entry
 //     db.Note.create(req.body)
-//       .then(function(dbNote) {
-//         // If a Note was created successfully, find one Article 
-//         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//       })
-//       .then(function(dbArticle) {
-//         // If we were able to successfully update an Article, send it back to the client
-//         res.json(dbArticle);
-//       })
-//       .catch(function(err) {
-//         // If an error occurred, send it to the client
-//         res.json(err);
-//       });
-//   });
+//         .then(function (dbNote) {
+//             // If a Note was created successfully, find one Article 
+//             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//         })
+//         .then(function (dbArticle) {
+//             // If we were able to successfully update an Article, send it back to the client
+//             res.json(dbArticle);
+//         })
+//         .catch(function (err) {
+//             // If an error occurred, send it to the client
+//             res.json(err);
+//         });
+// });
 
 
 
 
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
+});
