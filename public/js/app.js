c546aa75b4d3fc3f05ef45d763d96faafa68f8f4
Alicia Collins <alicia.collins60@me.com>
2018-08-27 18:49:53 -0700

    updated partials

--- public/js/app.js
+++ public/js/app.js
@@ -1,82 +1,56 @@
-// Grab the articles
-$("#myButton").on("click", function () {
-  console.log("yooooo");
-  $.getJSON("/scrape", function (err, data) {
-    if (err !== null) {
-      console.log(data)
-      window.location.reload();
-    } else {
-      alert('oops')
-    }
+
+$(function () {
+  $("#submit").on("click", function (event) {
+      console.log("I clicked")
+      // Grab the id associated with the article from the submit button
+      var thisId = $(this).attr("data-id");
+
+      // Post comment to article
+      $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+          // Value taken from note textarea
+          body: $("#bodyinput").val().trim()
+          
+        }
+        
+      })
+        // With that done
+        .then(function (data) {
+          // Log the response
+          console.log(data);
+          $("#bodyinput").val("");
+        });
+    
+      // Also, remove the values entered in the input and textarea for note entry
+     
   });
 });
-$.getJSON("/articles", function(data) {
 
-  for (var i = 0; i < data.length; i++) {
-    // id not working
-    console.log("id",data[i]._id)
-    $("#articles").append("<p data-id='" + data[i]._id + "'><b>" + data[i].title + "</b><br />" + data[i].summary + "<br />"+ data[i].link + "</p>");
-  }
-});
 
-//click p tag
-$(document).on("click", "p", function () {
-  $("#notes").empty();
-  //Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function (data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// When you click the savenote button
-$(document).on("click", "#save", function () {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
 
   // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
-  })
-    // With that done
-    .then(function (data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
-    });
+  
 
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-});
+
+// $("#myButton").on("click", function () {
+//   console.log("yooooo");
+//   $.getJSON("/scrape", function (err, data) {
+//     if (err !== null) {
+//       console.log(data)
+//       window.location.reload();
+//     } else {
+//       alert('oops')
+//     }
+//   });
+// });
+// $.getJSON("/articles", function(data) {
+
+//   for (var i = 0; i < data.length; i++) {
+//     // id not working
+//     console.log("id",data[i]._id)
+//     $("#articles").append("<p data-id='" + data[i]._id + "'><b>" + data[i].title + "</b><br />" + data[i].summary + "<br />"+ data[i].link + "</p>");
+//   }
+// });
+
--- server.js
+++ server.js
@@ -2,108 +2,83 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require("body-parser")
-var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require('cheerio');
+
+var db = require("./models");
+
+var PORT = process.env.PORT || 8000;
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapping";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
-// mongoose.connect("mongodb://localhost/Scrapping");
+mongoose.connect(MONGODB_URI) || "mongodb://localhost/Scrapping";
 
-var db = require("./models");
-
-var PORT = process.env.PORT || 8000;
 var app = express();
+app.use(express.static("public"));
 
-
-app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-app.use(express.static("public"));
 
 app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: "main" }));
 app.set('view engine', '.hbs');
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapping");
+// mongoose.connect("mongodb://localhost/scrapping");
 
 
-app.get("/scrape", function (req, res) {
+app.get("/", function (req, res) {
 
     axios.get("https://www.azcardinals.com/news/").then(function (response) {
-        const dbArticles = []
+        // const dbArticles = []
         var $ = cheerio.load(response.data);
 
         $(".d3-l-col__col-3").each(function (i, element) {
 
             var result = {};
             ///use this or element
-            result.title = $(element).find('a').attr("title");
-            result.link = "https://www.azcardinals.com/" + $(element).find('a').attr("href");
-            result.summary = $(element).find(".d3-o-media-object__summary").text().trim();
-            result.img = $(element).find("img").attr('src');
+            result.title = $(element)
+            .find('a')
+            .attr("title");
+            result.link = "https://www.azcardinals.com/" + $(element)
+            .find('a')
+            .attr("href");
+            result.summary = $(element)
+            .find(".d3-o-media-object__summary")
+            .text()
+            .trim();
+            result.img = $(element)
+            .find("img")
+            .attr('src');
 
-            dbArticles.push(db.Article.create(result));
-        });
+            // dbArticles.push(db.Article.create(result));
 
-        Promise.all(dbArticles).then(articles => {
-            console.log(articles);
-            res.send(articles);
-        }).catch(err => {
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+            .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+            })
+            .catch(function(err) {
+            // If an error occurred, send it to the client
             console.log(err);
+            });
         });
-    });
-});
-
-app.get("/", function (req, res) {
-    db.Article.find({})
-        .then(function (dbArticles) {
-            console.log("dbArticles", dbArticles);
-            res.render("index", { articles: dbArticles });
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
-
-
-app.get("/articles", function (req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
-        .then(function (dbArticle) {
-            // If we were able to successfully find Articles, send them back to the client
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
-
-//Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function (req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    console.log("findOne", { _id: req.params.id })
-    db.Article.findOne({ _id: req.params.id })
+        db.Article.find({})
         .populate("note")
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
+        .then(function(articles){
+            res.render("index", {articles:articles})
         });
-});
+
+    });
+})
+
+
 
 // This link is not working say undefined but is defined
 app.post("/articles/:id", function (req, res) {
@@ -121,11 +96,7 @@ app.post("/articles/:id", function (req, res) {
             // If an error occurred, send it to the client
             res.json(err);
         });
-});
-
-
-
-
+    });
 
 // Start the server
 app.listen(PORT, function () {
